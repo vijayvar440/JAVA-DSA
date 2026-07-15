e002b1ade9f333cbb2112954ad9bdf156661fec3
vijayvar440 <vijayvar123456@gmail.com>
2026-07-15 15:02:22 +0530

    String Qtion

---
+++ STREING/Qtion/compyar.java
@@ -0,0 +1,18 @@
+public class compyar {
+    public static void CopyarLargestStr(String Furut[]){
+        String largest = Furut[0];
+
+        for(int i =0; i<Furut.length;i++){
+            if (largest.compareTo(Furut[i]) < 0 ) {
+                largest = Furut[i];
+                
+            }
+        }
+
+        System.out.println(largest);
+    }
+    public static void main(String[] args) {
+        String fruts[] ={"apple","mango","orange"};
+        CopyarLargestStr(fruts);
+    }
+}
---
+++ STREING/Qtion/subStr.java
@@ -0,0 +1,7 @@
+public class subStr {
+    public static void main(String[] args) {
+        String name = "vijay var";
+
+        System.out.println(name.substring(5));
+    }
+}
