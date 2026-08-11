5074080fc1dadadded36ef7ff545293f087286af
PoojaMandal19 <poojamandal1905@gmail.com>
2026-08-11 09:14:16 +0530

    up to date

--- BACKTRAKING/change_arrey.java
+++ BACKTRAKING/change_arrey.java
@@ -1,18 +1,17 @@
 
 public class change_arrey {
-    public static void changearrey(int arrey [] ,int i , int val){
-        if(i == arrey.length){
+    public static void changearrey(int[] arrey, int i, int val) {
+        if (i == arrey.length) {
             return;
-        }   
+        }
 
+        int original = arrey[i];
         arrey[i] = val;
-        changearrey(arrey, i+1, val);
-        arrey[i] = arrey[i] - 2;
-
-        
+        changearrey(arrey, i + 1, val);
+        arrey[i] = original;
     }
 
-    public static void rintarrey(int arrey []) {
+    public static void rintarrey(int[] arrey) {
 
         for(int i = 0; i <arrey.length; i++){
             System.out.print(arrey[i] + " ");
